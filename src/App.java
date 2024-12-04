6cfa7a698019a8443b14469d9e1c36592360ad4d
Denisse <pestefania968@gamail.com>
2024-12-04 13:02:40 -0500

    Practica estructuras

--- src/App.java
+++ src/App.java
@@ -8,16 +8,29 @@ public class App {
     public static void runStack() {
         Stack stack = new Stack();
 
-        //Agregar elementos a la pila 
-        stack.push(5);
-        stack.push(7);
+        // Agregar elementos a la pila
+        System.out.println("Agregando elementos a la pila...");
         stack.push(10);
+        stack.push(6);
+        stack.push(8);
+        stack.push(11);
         stack.push(20);
 
-        System.out.println("Cima     -> "+stack.peek());
-        System.out.println("Retirar  -> "+stack.pop());
-        System.out.println("Cima     -> "+stack.peek());
-        System.out.println("Retirar  -> "+stack.pop());
-        System.out.println("Cima     -> "+stack.peek());
+        // Mostrar el estado de la pila después de las inserciones
+        stack.printStack();
+        System.out.println("Tamaño de la pila: " + stack.getSize());
+        System.out.println();
+
+        // Operaciones en la pila
+        System.out.println("Cima actual -> " + stack.peek());
+        System.out.println("Retirar elemento -> " + stack.pop());
+        System.out.println("Nueva cima -> " + stack.peek());
+        System.out.println("Retirar elemento -> " + stack.pop());
+        System.out.println("Nueva cima -> " + stack.peek());
+
+        // Mostrar el estado de la pila después de las operaciones
+        System.out.println();
+        stack.printStack();
+        System.out.println("Tamaño final de la pila: " + stack.getSize());
     }
 }
